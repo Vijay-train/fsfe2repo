0224f3aee46b8f849c6dd2c622280a48d7e3ff33
Vijay-train <53718047+Vijay-train@users.noreply.github.com>
2022-03-18 06:11:16 +0000

    added /demo path

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ app.get('/',(req,res) => {
 	res.send('Hello world! from node');
 });
 
+app.get('/demo',(req,res) => {
+	res.send('demo path');
+	res.status(418);
+	res.set('X-full-stack','for life');
+});
+
 app.listen(port,() => console.log('code space changes 2 example app listneing on port {port}!'));
